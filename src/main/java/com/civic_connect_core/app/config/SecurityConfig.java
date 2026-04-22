cdb917260d278289bcd470dbc1fbce8df389987f
aniket <loharaniket2003@gmail.com>
2026-04-22 23:38:37 +0530

    security: Impliment Spring Security

---
+++ src/main/java/com/civic_connect_core/app/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.civic_connect_core.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.civic_connect_core.app.filters.JwtFilter;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**").permitAll()
+                        // .requestMatchers("/api/v1/users/me").permitAll()
+                        .requestMatchers("/api/v1/users/me").hasAnyRole("SUPER_ADMIN", "DEPT_ADMIN", "CITIZEN")
+                        .requestMatchers("/api/v1/users/**").hasAnyRole("SUPER_ADMIN")
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
