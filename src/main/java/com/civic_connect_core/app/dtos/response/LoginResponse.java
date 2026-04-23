6aebdc8abdb14de71d57c57ab53d2741c0ba15a2
aniket <loharaniket2003@gmail.com>
2026-04-23 22:49:56 +0530

    feat(jwt): return JWT on successful login

---
+++ src/main/java/com/civic_connect_core/app/dtos/response/LoginResponse.java
@@ -0,0 +1,8 @@
+package com.civic_connect_core.app.dtos.response;
+
+import lombok.Data;
+
+@Data
+public class LoginResponse {
+    private final String token;
+}
--- src/main/java/com/civic_connect_core/app/service/AuthService.java
+++ src/main/java/com/civic_connect_core/app/service/AuthService.java
@@ -2,19 +2,26 @@ package com.civic_connect_core.app.service;
 
 import java.time.LocalDateTime;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.civic_connect_core.app.dtos.AuthDTO.AuthRequest;
+import com.civic_connect_core.app.dtos.response.LoginResponse;
 import com.civic_connect_core.app.entity.Users;
+import com.civic_connect_core.app.enums.UserRoles;
 import com.civic_connect_core.app.exceptions.UserAlreadyExistException;
+import com.civic_connect_core.app.exceptions.UserNotFoundException;
 import com.civic_connect_core.app.repository.UsersRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class AuthService {
     private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public Users register(AuthRequest request) {
         if (usersRepository.existsByUserEmail(request.getUserEmail())) {
@@ -22,11 +29,28 @@ public class AuthService {
         }
         var user = Users.builder()
                 .userEmail(request.getUserEmail())
-                .userPassword(request.getUserPassword())
+                .userPassword(passwordEncoder.encode(request.getUserPassword()))
                 .userRole("CITIZEN")
                 .createdAt(LocalDateTime.now())
                 .build();
         usersRepository.save(user);
+        return user;
+    }
+
+    public LoginResponse login(AuthRequest request) {
+        var user = usersRepository.findByUserEmail(request.getUserEmail())
+                .orElseThrow(() -> new UserNotFoundException("Invalid Credentials"));
+        if (!passwordEncoder.matches(request.getUserPassword(), user.getUserPassword())) {
+            throw new UserNotFoundException("Invalid Password");
+        }
+        var response = new LoginResponse(jwtService.generateToken(user.getUserId(), user.getUserRole()));
+        return response;
+    }
+
+    @Transactional
+    public Users changeUserRole(Long userId, UserRoles role) {
+        var user = usersRepository.findByUserId(userId).orElseThrow(() -> new UserNotFoundException("User Not Found"));
+        user.setUserRole(role.name());
         return user;
     }
 
